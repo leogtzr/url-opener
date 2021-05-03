93146bd990fb85cf86068dcbba5943576efb2253
leogtzr <leogutierrezramirez@gmail.com>
2021-05-03 07:36:51 -0600

    add logic to filter based on CLI args and without them

--- urlop.sh
+++ urlop.sh
@@ -11,52 +11,53 @@ readonly error_missing_url_lst_file=83
 readonly work_tmp_file="${work_dir}/url.tmp"
 
 clean_up() {
-	if [[ -f "${work_tmp_file}" ]]; then
-		rm --force "${work_tmp_file}" > /dev/null >&2
-	fi
+    if [[ -f "${work_tmp_file}" ]]; then
+        rm --force "${work_tmp_file}" > /dev/null >&2
+    fi
 }
 
 trap clean_up ERR EXIT SIGINT SIGTERM
 
 if [[ ! -f "${conf_file}" ]]; then
-	echo "error: ${conf_file} not found" >&2
-	exit ${error_conf_file_not_found}
+    echo "error: ${conf_file} not found" >&2
+    exit ${error_conf_file_not_found}
 fi
 
 . "${conf_file}" || {
-	echo "error: reading ${conf_file}" >&2
-	exit ${error_reading_conf_file}
+    echo "error: reading ${conf_file}" >&2
+    exit ${error_reading_conf_file}
 }
 
 if [[ -z "${BROWSER}" ]]; then
-	echo "error: BROWSER env variable not set" >&2
-	exit ${error_missing_env_bar}
+    echo "error: BROWSER env variable not set" >&2
+    exit ${error_missing_env_bar}
 fi
 
 if [[ -z "${url_list_file}" ]]; then
-	echo "error: missing 'url_list_file'" >&2
-	exit ${error_missing_env_bar}
+    echo "error: missing 'url_list_file'" >&2
+    exit ${error_missing_env_bar}
 fi
 
 if [[ ! -f "${url_list_file}" ]]; then
-	echo "error: ${url_list_file} not found" >&2
-	exit ${error_missing_url_lst_file}
+    echo "error: ${url_list_file} not found" >&2
+    exit ${error_missing_url_lst_file}
 fi
 
-for arg in ${@}; do
-	echo "${arg}" >> "${work_tmp_file}"
-done
-
-readonly ocurrences_search_text=$(grep --ignore-case --file "${work_tmp_file}" "${url_list_file}" | \
-	dmenu -l 30 -nb "#100" -nf "#b9c0af" -sb "#000" -sf "#afff2f" -i)
-
-if [[ -z "${ocurrences_search_text}" ]]; then
-	exit 0
+if ((${#} > 0)); then
+    for arg in ${@}; do
+        echo "${arg}" >> "${work_tmp_file}"
+    done
+    readonly ocurrences_search_text=$(grep --ignore-case --file "${work_tmp_file}" "${url_list_file}" | \
+        dmenu -l 40 -nb "#100" -nf "#b9c0af" -sb "#000" -sf "#afff2f" -i)
+else
+    readonly ocurrences_search_text=$(cat "${url_list_file}" | dmenu -l 40 -sf "#afff2f" -i)
 fi
+
+[[ -z "${ocurrences_search_text}" ]] && exit 0
 
 readonly url_to_open=$(awk '{print $1}' <<< $(echo "${ocurrences_search_text}"))
-"${BROWSER}" "${url_to_open}" > /dev/null >&2
 
-printf "Args n: %d\n" ${#}
+# Open the filtered URL:
+"${BROWSER}" "${url_to_open}" > /dev/null >&2
 
 exit 0
